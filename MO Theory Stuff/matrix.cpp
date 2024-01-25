9aa38037126d5aaf4d5a25fea58a67d6ebbc6630
Prioski <luca.sean.thomas.morgan@gmail.com>
2024-01-25 17:54:02 +0000

    Vectors<n> + some of Matrix<n>

--- MO Theory Stuff/matrix.cpp
+++ MO Theory Stuff/matrix.cpp
@@ -1,6 +1,112 @@
 #include <iostream>
 #include <array>
 
+template<int n>
+class Vector
+{
+private:
+    std::array<int, n> vec{};
+public:
+    Vector(std::array<int, n> input = std::array<int, n>{})
+    {
+        for (int i = 0; i < n; i++)
+            vec[i] = input[i];
+    }
+
+    void print()
+    {
+        std::cout << "(";
+        for (int i = 0; i < n; i++)
+        {
+            if (i == n - 1)
+                std::cout << vec[i];
+            else
+                std::cout << vec[i] << ", ";
+        }
+        std::cout << ")" << '\n';
+    }
+
+    int operator[](int obj) const
+    {
+        return vec[obj];
+    }
+
+    int& operator[](int obj)
+    {
+        return vec[obj];
+    }
+
+    Vector<n> operator+(Vector<n> obj)
+    {
+        Vector<n> res;
+        for (int i = 0; i < n; i++)
+            res[i] = vec[i] + obj[i];
+        return res;
+    }
+
+    bool operator==(Vector<n> obj)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (vec[i] != obj[i])
+                return false;
+        }
+        return true;
+    }
+    Vector<n> operator=(Vector<n> obj)
+    {
+        for (int i = 0; i < n; i++)
+            vec[i] = obj[i];
+    }
+};
+
+template<int n>
+class Matrix
+{
+private:
+    std::array<std::array<int, n>, n> mat{};
+public:
+    Matrix(std::array<std::array<int, n>, n> input = std::array<std::array<int, n>, n>{})
+    {
+        for (int i=0; i < n; i++)
+        {
+            for (int j=0; j < n; j++)
+                mat[i][j] = input[j][i];
+        }
+    }
+
+    std::array<int, n> operator[](int obj) const
+    {
+        return mat[obj];
+    }
+    std::array<int, n>& operator[](int obj)
+    {
+        return mat[obj];
+    }
+
+    void print()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+                std::cout << mat[j][i] << " ";
+            std::cout << '\n';
+        }
+    }
+
+    Matrix<n> operator+(Matrix<n> obj)
+    {
+        Matrix<n> res;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+                res[i][j] = obj[i][j] + mat[i][j];
+        }
+        return res;
+    }
+};
+
+
 class Vector2D
 {
 private:
@@ -99,9 +205,14 @@ public:
 
 int main()
 {
-    Vector2D vect(1, 2);
-    Matrix2D matrix(0, 1, 1, 0);
-    Vector2D result = matrix * vect;
-    result.print();
+    //Vector<5> vec{ {0, 1, 3, 4, 2} };
+    //Vector<5> vec2{};
+    //Vector<5> res{ vec + vec2 };
+    //res.print();
+
+    Matrix<2> mat{ {1, 2, 3, 4} };
+    Matrix<2> mat2{ {10, 20, 30, 40} };
+    Matrix<2> res = mat + mat2;
+    res.print();
     return 0;
 }
