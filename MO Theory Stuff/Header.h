8baa357e241bf09f1c4dc9258b36f6f203de71df
Prioski <luca.sean.thomas.morgan@gmail.com>
2024-01-29 22:22:50 +0000

    Added Octahedral identification

--- MO Theory Stuff/Header.h
+++ MO Theory Stuff/Header.h
@@ -8,5 +8,6 @@ Group trigonal_planar_identifier(Vector<3> vec);
 Group linear_identifier(Vector<2> vec);
 Group tetrahedral_identifier(Vector<4> vec);
 Group trigonal_bipyramidal_identifier(Vector<5> vec);
+Group octahedral_identifier(Vector<6> vec);
 
 #endif
